f18b25248d43be731ce78c4f2c6fbac237dc4ad8
Juliano <alvarenga.vix@gmail.com>
2024-05-29 17:36:00 -0300

    Initial Commit

---
+++ FlickApp/FlickAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickAppApp.swift
+//  FlickApp
+//
+//  Created by Juliano Alvarenga on 29/05/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
